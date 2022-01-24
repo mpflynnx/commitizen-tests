1c2f45995d0a8847582c5c956f692f1b1692c523
mpflynnx <mpflynnx@gmail.com>
2022-01-24 10:47:15 +0000

    bump: version 0.0.0 → 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## v0.1.0 (2022-01-24)
+
+### Feat
+
+- **initial-commit**: inital commit
--- engineervix_test/__init__.py
+++ engineervix_test/__init__.py
@@ -4,4 +4,4 @@ Please refer to the documentation provided in the README.md
 
 __author__ = """mpflynnx"""
 __email__ = "mpflynnx@gmail.com"
-__version__ = "0.0.0"
+__version__ = "0.1.0"
